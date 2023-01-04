9cd2481087093b8f8d41b527678bcc9668a81f78
hjjin <hjjin@DESKTOP-8FP6VFD>
2023-01-04 16:44:59 +0900

    회원정보 DAO, VO

---
+++ skycoding/src/main/java/kr/hmember/dao/MemberDAO.java
@@ -0,0 +1,84 @@
+package kr.hmember.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import kr.hmember.vo.MemberVO;
+import kr.util.DBUtil;
+
+public class MemberDAO {
+	
+	//싱글턴 패턴
+	private static MemberDAO instance = new MemberDAO();
+	
+	public static MemberDAO getInstance() {
+		return instance;
+	}
+	
+	private MemberDAO() {}
+	
+	//회원가입
+	public void insertMember(MemberVO hmember)throws Exception{
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		PreparedStatement pstmt2 = null;
+		PreparedStatement pstmt3 = null;
+		ResultSet rs = null;
+		String sql = null;
+		int num = 0; //시퀀스 번호 저장
+		
+		try {
+			//커넥션 풀로부터 커넥션 할당
+			conn = DBUtil.getConnection();
+			
+			conn.setAutoCommit(false);
+			
+			sql = "SELECT hmember_seq.nextval FROM dual";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				num = rs.getInt(1);
+			}
+			
+			
+			sql = "INSERT INTO hmember (mem_num,mem_id) VALUES (?,?)";
+			pstmt2 = conn.prepareStatement(sql);
+			pstmt2.setInt(1, num);//회원번호
+			pstmt2.setString(2, hmember.getMem_id());//회원id
+			pstmt2.executeUpdate();
+			
+			
+			sql = "INSERT INTO hmember_detail (mem_num,mem_name,mem_pw,"
+					+ "mem_pwq,mem_pwa,mem_cell) VALUES (?,?,?,?,?,?)";
+			pstmt3 = conn.prepareStatement(sql);
+			pstmt3.setInt(1, num);//회원번호
+			pstmt3.setString(2, hmember.getMem_name());
+			pstmt3.setString(3, hmember.getMem_pw());
+			pstmt3.setInt(4, hmember.getMem_pwq());
+			pstmt3.setString(5, hmember.getMem_pwa());
+			pstmt3.setString(6, hmember.getMem_cell());
+			pstmt3.executeUpdate();
+			
+			//모두 성공 시 commit
+			conn.commit();
+		}catch(Exception e) {
+			//하나라도 실패 시 rollback
+			conn.rollback();
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(null, pstmt3, conn);
+			DBUtil.executeClose(null, pstmt2, conn);
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+	}	
+}
+
+
+
+
+
+
+
+
---
+++ skycoding/src/main/java/kr/hmember/vo/MemberVO.java
@@ -0,0 +1,88 @@
+package kr.hmember.vo;
+
+import java.sql.Date;
+
+public class MemberVO {
+	private int mem_num;
+	private String mem_id;
+	private int mem_auth;
+	private String mem_name;
+	private String mem_pw;
+	private int mem_pwq;
+	private String mem_pwa;
+	private String mem_cell;
+	private Date mem_reg_date;
+	private Date mem_modify_date;
+	
+	
+	//비밀번호 일치 여부 체크
+	public boolean isCheckedPassword(String userPasswd) {
+		//회원 등급(mem_auth): 0-탈퇴회원, 1-일반회원, 9-관리자
+		if(mem_auth > 0 && mem_pw.equals(userPasswd)) {
+			//비밀번호 일치 시
+			return true;
+		}
+			return false;
+	}
+	
+	public int getMem_num() {
+		return mem_num;
+	}
+	public void setMem_num(int mem_num) {
+		this.mem_num = mem_num;
+	}
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public int getMem_auth() {
+		return mem_auth;
+	}
+	public void setMem_auth(int mem_auth) {
+		this.mem_auth = mem_auth;
+	}
+	public String getMem_name() {
+		return mem_name;
+	}
+	public void setMem_name(String mem_name) {
+		this.mem_name = mem_name;
+	}
+	public String getMem_pw() {
+		return mem_pw;
+	}
+	public void setMem_pw(String mem_pw) {
+		this.mem_pw = mem_pw;
+	}
+	public int getMem_pwq() {
+		return mem_pwq;
+	}
+	public void setMem_pwq(int mem_pwq) {
+		this.mem_pwq = mem_pwq;
+	}
+	public String getMem_pwa() {
+		return mem_pwa;
+	}
+	public void setMem_pwa(String mem_pwa) {
+		this.mem_pwa = mem_pwa;
+	}
+	public String getMem_cell() {
+		return mem_cell;
+	}
+	public void setMem_cell(String mem_cell) {
+		this.mem_cell = mem_cell;
+	}
+	public Date getMem_reg_date() {
+		return mem_reg_date;
+	}
+	public void setMem_reg_date(Date mem_reg_date) {
+		this.mem_reg_date = mem_reg_date;
+	}
+	public Date getMem_modify_date() {
+		return mem_modify_date;
+	}
+	public void setMem_modify_date(Date mem_modify_date) {
+		this.mem_modify_date = mem_modify_date;
+	}
+}

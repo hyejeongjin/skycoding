2ca4d84b0d60c3399c9471edb0fac09fb9ddd103
shkim <shkim_114@naver.com>
2023-01-04 16:15:25 +0900

    강의 좋아요/강의 자바빈 생성

---
+++ skycoding/src/main/java/kr/course/vo/CourseFavVO.java
@@ -0,0 +1,26 @@
+package kr.course.vo;
+
+
+public class CourseFavVO {
+	private int like_num;
+	private int couse_id;
+	private int mem_num;
+	public int getLike_num() {
+		return like_num;
+	}
+	public void setLike_num(int like_num) {
+		this.like_num = like_num;
+	}
+	public int getCouse_id() {
+		return couse_id;
+	}
+	public void setCouse_id(int couse_id) {
+		this.couse_id = couse_id;
+	}
+	public int getMem_num() {
+		return mem_num;
+	}
+	public void setMem_num(int mem_num) {
+		this.mem_num = mem_num;
+	}
+}
---
+++ skycoding/src/main/java/kr/course/vo/CourseVo.java
@@ -0,0 +1,71 @@
+package kr.course.vo;
+
+import java.sql.Date;
+
+public class CourseVo {
+	//컬럼명과 일치하게 만들어주기 , 데이터와 맵핑
+	   private int course_id;
+	   private int mem_num;
+	   private String course_name;
+	   private int course_hit;
+	   private String course_content;
+	   private Date report_date;
+	   private String course_tr;
+	   private int course_cate;
+	   private String course_photo;
+	public int getCourse_id() {
+		return course_id;
+	}
+	public void setCourse_id(int course_id) {
+		this.course_id = course_id;
+	}
+	public int getMem_num() {
+		return mem_num;
+	}
+	public void setMem_num(int mem_num) {
+		this.mem_num = mem_num;
+	}
+	public String getCourse_name() {
+		return course_name;
+	}
+	public void setCourse_name(String course_name) {
+		this.course_name = course_name;
+	}
+	public int getCourse_hit() {
+		return course_hit;
+	}
+	public void setCourse_hit(int course_hit) {
+		this.course_hit = course_hit;
+	}
+	public String getCourse_content() {
+		return course_content;
+	}
+	public void setCourse_content(String course_content) {
+		this.course_content = course_content;
+	}
+	public Date getReport_date() {
+		return report_date;
+	}
+	public void setReport_date(Date report_date) {
+		this.report_date = report_date;
+	}
+	public String getCourse_tr() {
+		return course_tr;
+	}
+	public void setCourse_tr(String course_tr) {
+		this.course_tr = course_tr;
+	}
+	public int getCourse_cate() {
+		return course_cate;
+	}
+	public void setCourse_cate(int course_cate) {
+		this.course_cate = course_cate;
+	}
+	public String getCourse_photo() {
+		return course_photo;
+	}
+	public void setCourse_photo(String course_photo) {
+		this.course_photo = course_photo;
+	}
+
+}

ca54f3c2aee1a6f6ce940bbfaa2265a96c33946a
hjjin <hjjin@DESKTOP-8FP6VFD>
2023-01-09 16:55:48 +0900

    로그인 폼 내용이 왜 없었을까요?

--- skycoding/src/main/java/kr/hmember/action/LoginFormAction.java
+++ skycoding/src/main/java/kr/hmember/action/LoginFormAction.java
@@ -1,5 +1,16 @@
 package kr.hmember.action;
 
-public class LoginFormAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.controller.Action;
+
+public class LoginFormAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		return "/WEB-INF/views/member/loginForm.jsp";
+	}
 
 }

d73d704dffa1eac91c599077e3c144beb25d89c3
kkonii <119216472+kkonii@users.noreply.github.com>
2023-01-10 01:20:53 +0900

    이벤트 테이블 수정

--- skycoding/src/main/webapp/sql/kkonii.sql
+++ skycoding/src/main/webapp/sql/kkonii.sql
@@ -2,23 +2,18 @@
 CREATE TABLE EVENT(
     event_id number,
     mem_num number not null,
+    event_course_id number not null,
     event_attr number(1) not null,
     event_deadline varchar2(10) not null,
+    event_reg_date date default SYSDATE not null,
     event_hit number not null,
-    event_content varchar2(4000) not null,
     event_photo varchar2(150) not null,
+    event_content varchar2(4000) not null,
+    event_detail_content varchar2(4000) not null,
     constraint event_pk primary key (event_id),
-    constraint event_fk foreign key (mem_num) references MEMBER (mem_num)
+    constraint event_fk1 foreign key (mem_num) references HMEMBER (mem_num),
+    constraint event_fk2 foreign key (event_course_id) references COURSE (course_id)
 );
 
-create sequence event_seq;
-
---이벤트 상세 페이지
-CREATE TABLE EVENT_DETAIL( 
-	event_id number,
-	course_id number not null,
-	event_detail_content varchar2(4000) not null,
-	constraint event_detail_pk primary key (event_id),
-	constraint event_detail_fk foreign key (event_id) REFERENCES EVENT(event_id),
-	constraint event_detail_fk2 foreign key (course_id) REFERENCES COURSE (course_id)
-);
+--event_id 시퀀스
+CREATE SEQUENCE EVENT_SEQ;

1fa7481ae34f02230481e95c2c0ea0dd68979157
jihyeon99 <jihyeon2474@nate.com>
2023-01-11 15:33:19 +0900

    서지현 - kr.mypage.action 커밋

---
+++ skycoding/src/main/java/kr/mypage/action/ModifyProfileFormAction.java
@@ -0,0 +1,31 @@
+package kr.mypage.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import kr.controller.Action;
+import kr.mypage.dao.MypageDAO;
+import kr.mypage.vo.MypageVO;
+
+public class ModifyProfileFormAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		HttpSession session = request.getSession();
+		Integer user_num = 
+				(Integer)session.getAttribute("user_num");
+		if(user_num==null) {//로그인이 되지 않은 경우
+			return "redirect:/member/loginForm.do";
+		}
+		//로그인 된 경우
+		MypageDAO dao = MypageDAO.getInstance();
+		MypageVO member = dao.getMember(user_num);
+		
+		request.setAttribute("member", member);
+		
+		return "/WEB-INF/views/mypage/modifyProfileForm.jsp";
+	}
+
+}

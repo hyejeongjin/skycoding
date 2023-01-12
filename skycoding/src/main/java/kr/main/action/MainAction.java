bef61d031043470195806e20ceb2219b8a37f0ae
hsg60 <hsg60@192.168.219.174>
2023-01-12 18:38:49 +0900

    기본세팅

--- skycoding/src/main/java/kr/main/action/MainAction.java
+++ skycoding/src/main/java/kr/main/action/MainAction.java
@@ -1,16 +1,26 @@
 package kr.main.action;
 
+import java.util.List;
+
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import kr.controller.Action;
+import kr.course.dao.CourseDAO;
+import kr.course.vo.CourseVO;
 
 public class MainAction implements Action{
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
-		//JSP 경로 반환
+		//강의 목록 메인에 보여지도록함
+		CourseDAO courseDAO = CourseDAO.getInstance();
+	
+		List<CourseVO> courseList = 
+				courseDAO.getListCourse(1, 5,null, null,1, "1");//표시 강의만 반환 //String sort값 가져와야됨 null(X)문자이므로 ""사용
+		request.setAttribute("courseList", courseList);
 		return "/WEB-INF/views/main/main.jsp";
 	}
 

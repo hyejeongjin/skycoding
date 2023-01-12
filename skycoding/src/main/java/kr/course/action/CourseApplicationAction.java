e6880c5e34b15051bc3dd11a457e190336be9f04
wjcho <wjcho@DESKTOP-4L17T9D>
2023-01-12 20:51:54 +0900

    강의 수정

--- skycoding/src/main/java/kr/course/action/CourseApplicationAction.java
+++ skycoding/src/main/java/kr/course/action/CourseApplicationAction.java
@@ -4,9 +4,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
+import kr.cart.dao.CartDAO;
+import kr.cart.vo.CartVO;
 import kr.controller.Action;
-
 
 
 public class CourseApplicationAction implements Action {
@@ -14,16 +14,21 @@ public class CourseApplicationAction implements Action {
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		HttpSession session = request.getSession();
-		Integer user_num = 
-				(Integer)session.getAttribute("mem_num");
+		Integer user_num = (Integer)session.getAttribute("mem_num");
 		if(user_num==null) {//로그인이 되지 않은 경우
-			return "redirect:/member/loginForm.do";
+			return "redirect:/hmember/loginForm.do";
 		}
+		
 		//로그인 된 경우
-		
 		int course_id = Integer.parseInt(request.getParameter("course_id"));
-		request.setAttribute("course_id", course_id);
 		
+		CartDAO dao = CartDAO.getInstance();
+		CartVO cart = new CartVO();
+		cart.setCourse_id(course_id);
+		cart.setMem_num(user_num);
+		dao.insertCart(cart);
+		
+		request.setAttribute("course_id", course_id);
 		return "/WEB-INF/views/course/application.jsp";
 	}
 

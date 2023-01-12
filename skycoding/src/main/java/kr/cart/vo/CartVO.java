fdd831f701dacdf2693a94195b0858b59803c6c4
shkim <shkim_114@naver.com>
2023-01-12 17:12:30 +0900

    김소희 마이페이지 학습중 강좌 VO 업

---
+++ skycoding/src/main/java/kr/cart/vo/CartVO.java
@@ -0,0 +1,36 @@
+package kr.cart.vo;
+
+import java.sql.Date;
+
+public class CartVO {
+	private int cart_num;
+	private int course_id;
+	private Date reg_date;
+	private int mem_num;
+	public int getCart_num() {
+		return cart_num;
+	}
+	public void setCart_num(int cart_num) {
+		this.cart_num = cart_num;
+	}
+	public int getCourse_id() {
+		return course_id;
+	}
+	public void setCourse_id(int course_id) {
+		this.course_id = course_id;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	public int getMem_num() {
+		return mem_num;
+	}
+	public void setMem_num(int mem_num) {
+		this.mem_num = mem_num;
+	}
+	
+
+}

06b0d447158bcc6fa2995611e3d0274fea5bc783
kkonii <119216472+kkonii@users.noreply.github.com>
2023-01-13 12:29:47 +0900

    원경연 - 등록글 수정 -> 수정폼 이동 로직

---
+++ skycoding/src/main/java/kr/event/action/UpdateEventFormAction.java
@@ -0,0 +1,28 @@
+package kr.event.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.controller.Action;
+import kr.event.dao.EventDAO;
+import kr.event.vo.EventVO;
+import kr.util.StringUtil;
+
+public class UpdateEventFormAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setCharacterEncoding("utf-8");
+		
+		Integer event_id = Integer.parseInt(request.getParameter("event_id"));
+		EventDAO eventDao = EventDAO.getInstance();
+		EventVO event = eventDao.getDetailEvent(event_id);
+		event.setEvent_content(StringUtil.useNoHtml(event.getEvent_content()));
+		event.setEvent_detail_content(StringUtil.useBrNoHtml(event.getEvent_detail_content()));
+		
+		request.setAttribute("event", event);
+		
+		return "/WEB-INF/views/event/updateEventForm.jsp";
+	}
+
+}

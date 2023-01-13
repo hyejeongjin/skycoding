78a0a6b92988bdeb2690fe6e01022627a479d6f1
kkonii <119216472+kkonii@users.noreply.github.com>
2023-01-13 12:17:29 +0900

    원경연 - 게시글 조회수 증가, html태그 처리 기능 구현

--- skycoding/src/main/java/kr/event/action/EventDetailAction.java
+++ skycoding/src/main/java/kr/event/action/EventDetailAction.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import kr.controller.Action;
 import kr.event.dao.EventDAO;
 import kr.event.vo.EventVO;
+import kr.util.StringUtil;
 
 public class EventDetailAction implements Action{
 
@@ -16,9 +17,16 @@ public class EventDetailAction implements Action{
 		Integer event_id = Integer.parseInt(request.getParameter("event_id"));
 		EventDAO dao = EventDAO.getInstance();
 		
+		//누르는 순간 조회수 증가
+		dao.addEventView(event_id);
+		
 		//event_id에 해당하는 게시글 - 마감일까지 남은 일수 추출
 		EventVO event = dao.getDetailEvent(event_id);
 		Integer event_diffDay = dao.getDiffDate(event_id);
+		
+		//요약내용, 세부내용 StringUtil처리
+		event.setEvent_content(StringUtil.useNoHtml(event.getEvent_content()));
+		event.setEvent_detail_content(StringUtil.useBrNoHtml(event.getEvent_detail_content()));
 		
 		request.setAttribute("event", event);
 		request.setAttribute("event_diffDay", event_diffDay);

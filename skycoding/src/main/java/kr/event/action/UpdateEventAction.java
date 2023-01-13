d2f809de94d6fa72b397f0ae1739205a4f5b2a06
kkonii <119216472+kkonii@users.noreply.github.com>
2023-01-13 12:31:33 +0900

    원경연- 이벤트글 수정 기능

---
+++ skycoding/src/main/java/kr/event/action/UpdateEventAction.java
@@ -0,0 +1,45 @@
+package kr.event.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.oreilly.servlet.MultipartRequest;
+
+import kr.controller.Action;
+import kr.event.dao.EventDAO;
+import kr.event.vo.EventVO;
+import kr.util.FileUtil;
+
+public class UpdateEventAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setCharacterEncoding("utf-8");
+		MultipartRequest multi = FileUtil.createFile(request);
+		
+		Integer event_id = Integer.parseInt(multi.getParameter("event_id"));
+		
+		EventDAO dao = EventDAO.getInstance();
+		EventVO db_event = dao.getEvent(event_id);
+		
+		String event_file = multi.getFilesystemName("photo");
+		
+		EventVO event = new EventVO();
+		event.setEvent_id(event_id);
+		event.setEvent_attr(Integer.parseInt(multi.getParameter("attr")));
+		event.setEvent_deadline(multi.getParameter("deadline"));
+		event.setEvent_photo(event_file);
+		event.setEvent_content(multi.getParameter("content"));
+		event.setEvent_detail_content(multi.getParameter("detail_content"));
+		
+		dao.updateEvent(event);
+		
+		if(event_file != null) {
+			//새로운 파일을 선택했을 때 기존에 있던 파일 경로는 삭제
+			FileUtil.removeFile(request, db_event.getEvent_photo());
+		}
+		
+		return "/WEB-INF/views/event/updateEvent.jsp";
+	}
+
+}
